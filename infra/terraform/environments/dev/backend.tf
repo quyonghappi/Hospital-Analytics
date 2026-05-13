e02c7bb27c2dadc524f7f6ad97288c811eaa39ad
manh <163654667+quyonghappi@users.noreply.github.com>
2026-05-13 13:06:52 +0700

    Delete infra/terraform directory

--- infra/terraform/environments/dev/backend.tf
+++
@@ -1,6 +0,0 @@
-terraform {
-  backend "gcs" {
-    bucket = "project-8e2366a6-d3cc-40ee-9de-tf-state"
-    prefix = "terraform/state/dev/data-platform"
-  }
-}
--- infra/terraform/environments/dev/terraform.tfvars
+++
@@ -1,3 +0,0 @@
-project_id = "project-8e2366a6-d3cc-40ee-9de"
-region     = "asia-southeast1"
-env        = "dev"
